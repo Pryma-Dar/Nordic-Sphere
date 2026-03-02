1cf2d811304a211821964fcf748f04f27c8d4894
Daryna <basanskayamarina19@gmail.com>
2026-03-02 18:48:27 +0100

    Initial commit: complete Nortic Sphere database structure and test data

---
+++ README.md
@@ -0,0 +1,7 @@
+Project Name: Nordic Sphere E-commerce Analytics.
+
+Tech Stack: PostgreSQL.
+
+Objective: Simulated business analysis for a Scandinavian furniture retailer.
+
+Link to Portfolio: https://nordic-sphere-data-simulation.notion.site/Nordic-Sphere-Business-Analytics-Case-Study-313ae4652f6680ed868fd0acd48e4755
--- init_db.sql
+++ init_db.sql
@@ -67,11 +67,7 @@ INSERT INTO orders VALUES (5002, 2, 102, '2026-02-02', 2);
 
 ALTER TABLE customers ADD COLUMN second_name VARCHAR(50);
 
-UPDATE customers
-SET
-    second_name = 'McDonald'
-WHERE
-    customer_id = 1;
+UPDATE customers SET second_name = 'McDonald' WHERE customer_id = 1;
 
 UPDATE customers SET second_name = 'Bond' WHERE customer_id = 2;
 
@@ -89,3 +85,23 @@ ADD COLUMN margin_percent numeric GENERATED ALWAYS AS (
 ) STORED;
 
 UPDATE products SET sale_price = 80 WHERE product_id = 102;
+
+/* Added data for returns and new sales*/
+
+INSERT INTO returns (return_id, order_id, return_reason)
+VALUES 
+(901, 5002, 'Wrong color'),
+(902, 5002, 'Damaged during transport'),
+(903, 5003, 'Damaged during transport');
+
+/INSERT INTO customers (customer_id, first_name, country , segment, second_name)
+VALUES
+(3, 'Rachel', 'Germany', 'B2C', 'Green'),
+(4, 'Phybie', 'France', 'B2C', 'Bufe'),
+(5, 'Monica', 'Sweden', 'B2C', 'Gilmore');
+
+INSERT INTO orders (order_id, customer_id, product_id, order_date, quantity)
+VALUES
+(5003, 3, 103, '2026-03-02', 1),
+(5004, 4, 102, '2026-03-02', 2),
+(5005, 5, 101, '2026-03-02', 1);
