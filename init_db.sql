e72b62bd84b13410dac9f29ad0a3fef835b8dfe0
Daryna <basanskayamarina19@gmail.com>
2026-03-10 14:38:07 +0100

    Adding the initial_stock and new orders

--- init_db.sql
+++ init_db.sql
@@ -27,43 +27,26 @@ CREATE TABLE returns (
     return_reason VARCHAR(255)
 );
 
-INSERT INTO
-    products
-VALUES (
-        101,
-        'Malmo Sofa',
-        'Furniture',
-        400.00,
-        850.00
-    );
+INSERT INTO products
+VALUES (101, 'Malmo Sofa', 'Furniture', 400.00, 850.00 );
 
-INSERT INTO
-    products
-VALUES (
-        102,
-        'Oslo Lamp',
-        'Lighting',
-        25.00,
-        75.00
-    );
+INSERT INTO products
+VALUES ( 102, 'Oslo Lamp', 'Lighting', 25.00, 75.00 );
 
-INSERT INTO
-    products
-VALUES (
-        103,
-        'Wool Blanket',
-        'Decor',
-        15.00,
-        45.00
-    );
+INSERT INTO products
+VALUES ( 103, 'Wool Blanket', 'Decor',15.00, 45.00);
 
-INSERT INTO customers VALUES (1, 'Lukas', 'Germany', 'B2C');
+INSERT INTO customers 
+VALUES (1, 'Lukas', 'Germany', 'B2C');
 
-INSERT INTO customers VALUES (2, 'Emma', 'Sweden', 'B2C');
+INSERT INTO customers 
+VALUES (2, 'Emma', 'Sweden', 'B2C');
 
-INSERT INTO orders VALUES (5001, 1, 101, '2026-02-01', 1);
+INSERT INTO orders 
+VALUES (5001, 1, 101, '2026-02-01', 1);
 
-INSERT INTO orders VALUES (5002, 2, 102, '2026-02-02', 2);
+INSERT INTO orders 
+VALUES (5002, 2, 102, '2026-02-02', 2);
 
 ALTER TABLE customers ADD COLUMN second_name VARCHAR(50);
 
@@ -72,17 +55,13 @@ UPDATE customers SET second_name = 'McDonald' WHERE customer_id = 1;
 UPDATE customers SET second_name = 'Bond' WHERE customer_id = 2;
 
 ALTER TABLE products
-ADD COLUMN auto_margin DECIMAL(10, 2) GENERATED ALWAYS AS (sale_price - cost_price) STORED;
+ADD COLUMN auto_margin DECIMAL(10, 2) 
+GENERATED ALWAYS AS (sale_price - cost_price) STORED;
 
 ALTER TABLE products
-ADD COLUMN margin_percent numeric GENERATED ALWAYS AS (
-    ROUND(
-        (
-            (sale_price - cost_price) / sale_price
-        ) * 100,
-        2
-    )
-) STORED;
+ADD COLUMN margin_percent numeric 
+GENERATED ALWAYS AS (ROUND(((sale_price - cost_price) / sale_price ) * 100, 2 )
+        ) STORED;
 
 UPDATE products SET sale_price = 80 WHERE product_id = 102;
 
@@ -105,3 +84,36 @@ VALUES
 (5003, 3, 103, '2026-03-02', 1),
 (5004, 4, 102, '2026-03-02', 2),
 (5005, 5, 101, '2026-03-02', 1);
+
+ALTER TABLE products ADD COLUMN initial_stock INT;
+
+UPDATE products SET initial_stock = 50 WHERE product_id = 101; 
+UPDATE products SET initial_stock = 200 WHERE product_id = 102; 
+UPDATE products SET initial_stock = 150 WHERE product_id = 103;
+
+CREATE SEQUENCE orders_order_id_seq;
+ALTER TABLE orders 
+ALTER COLUMN order_id SET DEFAULT nextval('orders_order_id_seq');
+ALTER SEQUENCE orders_order_id_seq OWNED BY orders.order_id;
+SELECT setval('orders_order_id_seq', 5005);
+
+INSERT INTO orders (customer_id, product_id, order_date, quantity) VALUES
+
+(1, 101, '2026-02-01', 2),
+(2, 101, '2026-02-05', 1),
+(3, 101, '2026-02-10', 3),
+(4, 101, '2026-02-15', 2),
+(5, 101, '2026-02-20', 1),
+(1, 101, '2026-02-22', 2),
+(2, 101, '2026-02-25', 1),
+(3, 101, '2026-02-28', 1),
+
+(4, 103, '2026-02-03', 5),
+(5, 103, '2026-02-08', 3),
+(1, 103, '2026-02-12', 4),
+(2, 103, '2026-02-18', 6),
+(3, 103, '2026-02-21', 2),
+
+(4, 102, '2026-02-02', 2),
+(5, 102, '2026-02-07', 1),
+(1, 102, '2026-02-26', 1);
